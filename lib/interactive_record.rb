5721d1a0ae52ecf6d1b7af8466b7f2b2ac3d29d1
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-08 20:20:38 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,14 +17,16 @@ class InteractiveRecord
     #binding.pry
   end
   #binding.pry
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
 
   def initialize(attributes={})
     binding.pry
     attributes.each {|key, value| self.send(("#{key}="), value)}
 
   end
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
 
 end
