db73d4cb1e52a4fe489515e1324e3c2ef1912c69
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-08 19:46:27 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,9 +1,12 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
   def self.table_name
+    binding.pry
     self.downcase.pluralize
+    
   end
 end
